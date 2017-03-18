da472fe2aa60724469c7b71b177e1dfe0aaab74e
Noah Drabinsky <noah.drabinsky1@gmail.com>
2017-03-18 13:25:15 -0400

    Added a classes model and admin ability

--- StudlyApp/admin.py
+++ StudlyApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from StudlyApp.models import SchoolClasses
+
+class SchoolClassesAdmin(admin.ModelAdmin):
+    model = SchoolClasses
+    list_display = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(SchoolClasses, SchoolClassesAdmin)
