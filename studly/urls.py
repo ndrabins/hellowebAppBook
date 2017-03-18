11dc8279a715132d347aacd416b5f92984487057
Noah Drabinsky <noah.drabinsky1@gmail.com>
2017-03-18 17:09:16 -0400

    added registration, password reset functionality

--- studly/urls.py
+++ studly/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.conf.urls import url, include
+from django.contrib.auth.views import (
+   password_reset,
+   password_reset_done,
+   password_reset_confirm,
+   password_reset_complete
+)
 
 from StudlyApp import views
 
@@ -32,6 +39,29 @@ urlpatterns = [
     url(r'^course/(?P<slug>[-\w]+)/edit/$', views.edit_course,
         name='edit_course'),
 
+    url(r'^accounts/password/reset/$',
+        password_reset,
+        {'template_name':
+        'registration/password_reset_form.html'},
+        name="password_reset"),
+    url(r'^accounts/password/reset/done/$',
+        password_reset_done,
+        {'template_name':
+        'registration/password_reset_done.html'},
+        name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        password_reset_confirm,
+        {'template_name':
+        'registration/password_reset_confirm.html'},
+        name="password_reset_confirm"),
+    url(r'^accounts/password/done/$',
+        password_reset_complete,
+        {'template_name':
+        'registration/password_reset_complete.html'},
+        name="password_reset_complete"),
 
+
+    url(r'^accounts/',
+        include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
